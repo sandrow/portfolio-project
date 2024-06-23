94338a8ee72af555fa9275fbb324572cec621060
Sandrow <sandrow.townsend@hotmail.com>
2024-06-22 22:46:21 -0400

    started about section

---
+++ components/Grid.tsx
@@ -0,0 +1,20 @@
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {[{ title: "Title1", description: "Desc!", id: 1 }].map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
