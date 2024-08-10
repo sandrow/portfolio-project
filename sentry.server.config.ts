774c7c9230d270b13f22b04aa6b47de228041441
Sandrow <sandrow.townsend@hotmail.com>
2024-08-10 11:53:47 +0000

    resolved the hydration error & connected sentry to the site

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://40bbc6048643f640d20e2eb6c43ea6c7@o4507752774631424.ingest.us.sentry.io/4507752777908224",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
